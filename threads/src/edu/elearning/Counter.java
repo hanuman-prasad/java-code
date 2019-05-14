84ec1f49884588e9d48be77842d2f22b4e6340ae
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-05-14 07:19:29 +0530

    Reentrant lock unlock Interrupt example

---
+++ threads/src/edu/elearning/Counter.java
@@ -0,0 +1,22 @@
+package edu.elearning;
+
+public class Counter {
+
+    private int counter;
+
+    public void incCounter() {
+        this.counter++;
+    }
+
+    public void deccCounter() {
+        this.counter--;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
---
+++ threads/src/edu/elearning/Utils.java
@@ -0,0 +1,35 @@
+package edu.elearning;
+
+public class Utils {
+
+    public static final long MAX = 100L;
+
+    public static void sleep() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            printInterruptMessage("in sleep method");
+        }
+    }
+
+    public static void sleep(int sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            printInterruptMessage("in sleep method");
+        }
+    }
+
+    public static String threadName() {
+        return Thread.currentThread().getName();
+    }
+
+    public static void printMessage(String msg) {
+        System.out.println(threadName() + " " + msg);
+    }
+
+    public static void printInterruptMessage(String msg) {
+        printMessage("got interrupted " + msg + ". Interrupt status : " + Thread.currentThread().isInterrupted());
+    }
+
+}
