369007246799a0c7931763ae0019b0dc925cebcb
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-05-22 18:25:56 +0530

    recursive action task

---
+++ threads/src/edu/elearning/executor/RecursiveActionExample.java
@@ -0,0 +1,44 @@
+package edu.elearning.executor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+
+public class RecursiveActionExample extends RecursiveAction {
+
+    private String workload = "";
+    private static final int THRESHOLD = 4;
+
+
+    public RecursiveActionExample(String workload) {
+        this.workload = workload;
+    }
+
+    @Override
+    protected void compute() {
+        if (workload.length() > THRESHOLD) {
+            ForkJoinTask.invokeAll(createSubtasks());
+        } else {
+            processing(workload);
+        }
+    }
+
+    private List<RecursiveActionExample> createSubtasks() {
+        List<RecursiveActionExample> subtasks = new ArrayList<>();
+
+        String partOne = workload.substring(0, workload.length() / 2);
+        String partTwo = workload.substring(workload.length() / 2, workload.length());
+
+        subtasks.add(new RecursiveActionExample(partOne));
+        subtasks.add(new RecursiveActionExample(partTwo));
+
+        return subtasks;
+    }
+
+    private void processing(String work) {
+        String result = work.toUpperCase();
+        System.out.println("This result - (" + result + ") - was processed by "
+                + Thread.currentThread().getName());
+    }
+}
